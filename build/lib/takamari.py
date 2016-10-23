0736e4f9c3290265af1bc7e08a99a2fc3d798cbb
佐藤将道 <masamichi@INRI2.local>
2016-10-23 23:09:38 +0900

    slightly changed

---
+++ build/lib/takamari.py
@@ -0,0 +1,15 @@
+#! -*- encoding:utf-8 -*-
+"""nemui."""
+
+import sys
+
+from hello import HelloWorld
+
+
+def main(argv):
+    """majide nemui."""
+    hello = HelloWorld()
+    print hello.say_hello(argv[1])
+
+if __name__ == '__main__':
+    main(sys.argv)
